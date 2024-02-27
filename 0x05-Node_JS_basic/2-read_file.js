479bcb6d2b082bbcd2ecc4682a5c4642747461b5
OUALIID <walidelhadim8@gmail.com>
2024-02-27 15:37:33 +0100

    The countStudents function reads and analyzes CSV data, counting the number of students in both CS and SWE and displaying their names.

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,25 +1,32 @@
 const fs = require('fs');
 
 function countStudents(filePath) {
-  const data = fs.readFileSync(filePath, 'utf8').trim().split('\n');
-  let CS = 0, SWE = 0;
-  let csNames = [];
-  let SWENames = [];
+  try {
+    const data = fs.readFileSync(filePath, 'utf8').trim().split('\n');
+    let CS = 0, SWE = 0;
+    let csNames = [];
+    let SWENames = [];
 
-  for (let i = 1; i < data.length; i++) {
-    const columns = data[i].split(',');
-
-    if (columns[3] === 'CS') {
-      CS++;
-      csNames.push(columns[0]);
-    } else if (columns[3] === 'SWE') {
-      SWE++;
-      SWENames.push(columns[0]);
+    for (let i = 1; i < data.length; i++) {
+      const columns = data[i].split(',');
+  
+      if (columns[3] === 'CS') {
+        CS++;
+        csNames.push(columns[0]);
+      } else if (columns[3] === 'SWE') {
+        SWE++;
+        SWENames.push(columns[0]);
+      }
     }
+
+    const totalStudents = CS + SWE;
+    console.log(`Number of students: ${totalStudents}`);
+    console.log(`Number of students in CS: ${CS}. List: ${csNames.join(', ')}`);
+    console.log(`Number of students in SWE: ${SWE}. List: ${SWENames.join(', ')}`);
+  } catch (error) {
+    console.error('Cannot load the database');
+    throw new Error('Cannot load the database');
   }
-  console.log(`Number of students: ${CS + SWE}`)
-  console.log(`Number of students in CS: ${CS}. List: ${csNames.join(', ')}`)
-  console.log(`Number of students in SWE: ${SWE}. List: ${SWENames.join(', ')}`)
 }
 
 module.exports = countStudents;
