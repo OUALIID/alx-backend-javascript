3ed334578a6735d4d1c4df3324a543a8598481ef
OUALIID <walidelhadim8@gmail.com>
2024-02-27 20:44:28 +0100

    Added basic HTTP server functionality

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+const app = http.createServer((req, res) => {
+  // req : request and res : response
+  res.end("Hello Holberton School!");
+});
+
+app.listen(1245);
+module.exports = app;
