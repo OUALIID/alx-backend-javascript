a4fa4bc39f8cb736f1af9d17a36ff540eca7f2c3
OUALIID <walidelhadim8@gmail.com>
2024-02-27 21:57:09 +0100

    Moved the line This is our student list to be printed before the student list in the /students endpoint handler

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,59 +1,60 @@
 const http = require('http');
-const fs = require('fs');
+const { readFile } = require('fs').promises;
 
-function countStudents(filePath) {
-  return new Promise((resolve, reject) => {
-    try {
-      const data = fs.readFileSync(filePath, 'utf8').split('\n');
-      let NUMBER_OF_STUDENTS = 0;
-      const csNames = [];
-      const sweNames = [];
-      let CS = 0;
-      let SWE = 0;
-
-      for (let i = 1; i < data.length; i += 1) {
-        const columns = data[i].split(',');
-
-        if (columns.length >= 4) {
-          if (columns[3] === 'CS') {
-            CS += 1;
-            csNames.push(columns[0]);
-          } else if (columns[3] === 'SWE') {
-            SWE += 1;
-            sweNames.push(columns[0]);
-          }
-          NUMBER_OF_STUDENTS += 1;
+const countStudents = async () => {
+  try {
+    const fields = new Map();
+    let NumStd = 0;
+    const globalText = [];
+    let contents = await readFile(process.argv[2], 'utf8');
+    contents = contents.split('\n');
+    for (let i = 0; i < contents.length; i += 1) {
+      if (
+        contents[i].length > 0
+        && contents[i] !== 'firstname,lastname,age,field'
+      ) {
+        const data = contents[i].split(',');
+        if (fields.has(data[3]) === false) {
+          fields.set(data[3], []);
         }
+        fields.get(data[3]).push(data[0]);
+        NumStd += 1;
       }
-      const output = `
-Number of students: ${NUMBER_OF_STUDENTS}
-Number of students in CS: ${CS}. List: ${csNames.join(', ')}
-Number of students in SWE: ${SWE}. List: ${sweNames.join(', ')}`;
-      resolve(output);
-    } catch (error) {
-      console.error('Cannot load the database');
-      reject(new Error('Cannot load the database'));
     }
-  });
-}
-
-const app = http.createServer(async (req, res) => {
+    globalText.push(`Number of students: ${NumStd}`);
+    for (const [index, field] of fields.entries()) {
+      let text = field.join(', ');
+      text = `Number of students in ${index}: ${field.length}. List: ${text}`;
+      globalText.push(text);
+    }
+    return globalText;
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+};
+const hostname = '127.0.0.1';
+const port = 1245;
+const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   if (req.url === '/') {
     res.write('Hello Holberton School!');
     res.end();
-  } else if (req.url === '/students') {
-    try {
-      const data = await countStudents('database.csv');
-      res.write('This is the list of our students');
-      res.end(data);
-    } catch (error) {
-      res.statusCode = 404;
-      res.end(error.message);
-    }
+  }
+  if (req.url === '/students') {
+    res.write('This is the list of our students\n');
+    countStudents(res)
+      .then((data) => {
+        res.end(data.join('\n'));
+      })
+      .catch((err) => {
+        res.statusCode = 404;
+        res.end(err.message);
+      });
   }
 });
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
 
-app.listen(1245);
 module.exports = app;
