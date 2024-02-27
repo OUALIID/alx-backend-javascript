5062332c6ecc9b607970ace717e2441f73b50652
OUALIID <walidelhadim8@gmail.com>
2024-02-27 22:21:30 +0100

    More complex HTTP server using Express

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
+const { readFile } = require('fs').promises;
 
 const app = express();
 const port = 1245;
@@ -7,18 +7,41 @@ const port = 1245;
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
-
 app.get('/students', async (req, res) => {
+  const fields = new Map();
+  let NumStd = 0;
+  const globalText = ['This is the list of our students'];
   try {
-    const data = await countStudents(process.argv[2]);
-    res.send(`This is the list of our students\n${data}`);
+    let contents = await readFile(process.argv[2], 'utf8');
+    contents = contents.split('\n');
+    for (let i = 0; i < contents.length; i += 1) {
+      if (
+        contents[i].length > 0
+        && contents[i] !== 'firstname,lastname,age,field'
+      ) {
+        const data = contents[i].split(',');
+        if (fields.has(data[3]) === false) {
+          fields.set(data[3], []);
+        }
+        fields.get(data[3]).push(data[0]);
+        NumStd += 1;
+      }
+    }
+    globalText.push(`Number of students: ${NumStd}`);
+    for (const [index, field] of fields.entries()) {
+      let text = field.join(', ');
+      text = `Number of students in ${index}: ${field.length}. List: ${text}`;
+      globalText.push(text);
+    }
+    res.status(200).send(globalText.join('\n'));
   } catch (error) {
-    res.status(404).send(error.message);
+    globalText.push('Cannot load the database');
+    res.status(404).send(globalText.join('\n'));
   }
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
+  console.log(`Example app listening on port ${port}`);
 });
 
 module.exports = app;
