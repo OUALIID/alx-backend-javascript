def6969eb56a090633ad9a30b42f3ef9ab573b96
OUALIID <walidelhadim8@gmail.com>
2024-02-27 21:56:08 +0100

    Moved the line This is our student list to be printed before the student list in the /students endpoint handler

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,53 +1,52 @@
-const http = require("http");
-const { readFile } = require("fs").promises;
+const http = require('http');
+const fs = require('fs');
 
-const countStudents = async (filePath) => {
-  try {
-    const data = (await readFile(filePath, "utf8")).split("\n");
-    let NUMBER_OF_STUDENTS = 0;
-    const csNames = [];
-    const sweNames = [];
-    let CS = 0;
-    let SWE = 0;
+function countStudents(filePath) {
+  return new Promise((resolve, reject) => {
+    try {
+      const data = fs.readFileSync(filePath, 'utf8').split('\n');
+      let NUMBER_OF_STUDENTS = 0;
+      const csNames = [];
+      const sweNames = [];
+      let CS = 0;
+      let SWE = 0;
 
-    for (let i = 1; i < data.length; i += 1) {
-      const columns = data[i].split(",");
+      for (let i = 1; i < data.length; i += 1) {
+        const columns = data[i].split(',');
 
-      if (columns.length >= 4) {
-        if (columns[3] === "CS") {
-          CS += 1;
-          csNames.push(columns[0]);
-        } else if (columns[3] === "SWE") {
-          SWE += 1;
-          sweNames.push(columns[0]);
+        if (columns.length >= 4) {
+          if (columns[3] === 'CS') {
+            CS += 1;
+            csNames.push(columns[0]);
+          } else if (columns[3] === 'SWE') {
+            SWE += 1;
+            sweNames.push(columns[0]);
+          }
+          NUMBER_OF_STUDENTS += 1;
         }
-        NUMBER_OF_STUDENTS += 1;
       }
-    }
-
-    const output = `
+      const output = `
 Number of students: ${NUMBER_OF_STUDENTS}
-Number of students in CS: ${CS}. List: ${csNames.join(", ")}
-Number of students in SWE: ${SWE}. List: ${sweNames.join(", ")}`;
-
-    return output;
-  } catch (error) {
-    console.error("Cannot load the database");
-    throw new Error("Cannot load the database");
-  }
-};
+Number of students in CS: ${CS}. List: ${csNames.join(', ')}
+Number of students in SWE: ${SWE}. List: ${sweNames.join(', ')}`;
+      resolve(output);
+    } catch (error) {
+      console.error('Cannot load the database');
+      reject(new Error('Cannot load the database'));
+    }
+  });
+}
 
 const app = http.createServer(async (req, res) => {
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-
-  if (req.url === "/") {
-    res.write("Hello Holberton School!");
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
+    res.write('Hello Holberton School!');
     res.end();
-  } else if (req.url === "/students") {
+  } else if (req.url === '/students') {
     try {
-      res.write("This is the list of our students");
-      const data = await countStudents("database.csv");
+      const data = await countStudents('database.csv');
+      res.write('This is the list of our students');
       res.end(data);
     } catch (error) {
       res.statusCode = 404;
