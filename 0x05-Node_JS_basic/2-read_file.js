01a58b129300320d4cbf60cb36bc8b8bd3d44b14
OUALIID <walidelhadim8@gmail.com>
2024-02-27 17:59:10 +0100

    The countStudents function reads and analyzes CSV data, counting the number of students in both CS and SWE and displaying their names.

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -5,24 +5,29 @@ function countStudents (filePath) {
     const data = fs.readFileSync(filePath, 'utf8').trim().split('\n');
     let CS = 0;
     let SWE = 0;
+    let all = 0;
     const csNames = [];
     const sweNames = [];
 
     for (let i = 1; i < data.length; i++) {
       const columns = data[i].split(',');
 
-      if (columns[3] === 'CS') {
-        CS++;
-        csNames.push(columns[0]);
-      } else if (columns[3] === 'SWE') {
-        SWE++;
-        sweNames.push(columns[0]);
+      if (columns.length >= 4) {
+        if (columns[3] === 'CS') {
+          CS++;
+          csNames.push(columns[0]);
+        } else if (columns[3] === 'SWE') {
+          SWE++;
+          sweNames.push(columns[0]);
+        }
+        all++;
       }
     }
-    console.log(`Number of students: ${CS + SWE}`);
+    console.log(`Number of students: ${all}`);
     console.log(`Number of students in CS: ${CS}. List: ${csNames.join(', ')}`);
     console.log(`Number of students in SWE: ${SWE}. List: ${sweNames.join(', ')}`);
   } catch (error) {
+    // If the database is not available, throw an error
     console.error('Cannot load the database');
     throw new Error('Cannot load the database');
   }
