0880b3fc75dd7aad672bdea1b1a68e059f762ae5
OUALIID <walidelhadim8@gmail.com>
2024-02-27 22:20:07 +0100

    More complex HTTP server using Express

---
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', async (req, res) => {
+  try {
+    const data = await countStudents(process.argv[2]);
+    res.send(`This is the list of our students\n${data}`);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
+
+module.exports = app;
