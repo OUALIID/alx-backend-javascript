07ce72281dc1f4cf86769c96b32b484b8def54d7
OUALIID <walidelhadim8@gmail.com>
2024-02-27 20:19:28 +0100

    main file

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+
+function countStudents(filePath) {
+  return new Promise((resolve, reject) => {
+    try {
+      const data = fs.readFileSync(filePath, "utf8").split("\n");
+      let NUMBER_OF_STUDENTS = 0;
+      const csNames = [];
+      const sweNames = [];
+      let CS = 0;
+      let SWE = 0;
+
+      for (let i = 1; i < data.length; i += 1) {
+        const columns = data[i].split(",");
+        if (columns.length >= 4) {
+          if (columns[3] === "CS") {
+            CS += 1;
+            csNames.push(columns[0]);
+          } else if (columns[3] === "SWE") {
+            SWE += 1;
+            sweNames.push(columns[0]);
+          }
+          NUMBER_OF_STUDENTS += 1;
+        }
+      }
+      console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
+      console.log(`Number of students in CS: ${CS}. List: ${csNames.join(", ")}`);
+      console.log(`Number of students in SWE: ${SWE}. List: ${sweNames.join(", ")}`);
+      resolve();
+    } catch (error) {
+      console.error("Cannot load the database");
+      reject(new Error("Cannot load the database"));
+    }
+  });
+}
+
+module.exports = countStudents;
