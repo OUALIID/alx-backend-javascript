aa9e9ff5b8978361753a6976a47468558779483a
OUALIID <walidelhadim8@gmail.com>
2024-02-28 15:49:15 +0100

    update my code

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,65 +1,66 @@
 const http = require('http');
 const fs = require('fs');
 
+const fileName = process.argv[2];
+const host = 'localhost';
+
 function countStudents(filePath) {
-  try {
-    const data = fs.readFileSync(filePath, 'utf8').split('\n');
-    let NUMBER_OF_STUDENTS = 0;
-    const csNames = [];
-    const sweNames = [];
-    let CS = 0;
-    let SWE = 0;
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        console.error('Cannot load the database');
+        reject(new Error('Cannot load the database'));
+      } else {
+        const rows = data.split('\n');
+        let NUMBER_OF_STUDENTS = 0;
+        const csNames = [];
+        const sweNames = [];
+        let CS = 0;
+        let SWE = 0;
 
-    for (let i = 1; i < data.length; i += 1) {
-      const columns = data[i].split(',');
+        for (let i = 1; i < rows.length; i++) {
+          const columns = rows[i].split(',');
 
-      if (columns.length >= 4) {
-        if (columns[3] === 'CS') {
-          CS += 1;
-          csNames.push(columns[0]);
-        } else if (columns[3] === 'SWE') {
-          SWE += 1;
-          sweNames.push(columns[0]);
+          if (columns.length >= 4) {
+            if (columns[3] === 'CS') {
+              CS++;
+              csNames.push(columns[0]);
+            } else if (columns[3] === 'SWE') {
+              SWE++;
+              sweNames.push(columns[0]);
+            }
+            NUMBER_OF_STUDENTS++;
+          }
         }
-        NUMBER_OF_STUDENTS += 1;
+
+        const responseBody = `This is the list of our students
+Number of students: ${NUMBER_OF_STUDENTS}
+Number of students in CS: ${CS}. List: ${csNames.join(', ')}
+Number of students in SWE: ${SWE}. List: ${sweNames.join(', ')}`;
+        resolve(responseBody);
       }
-    }
-
-    const responseData = {
-      NUMBER_OF_STUDENTS,
-      csNames,
-      sweNames
-    };
-
-    return responseData;
-  } catch (error) {
-    console.error('Cannot load the database');
-    throw new Error('Cannot load the database');
-  }
+    });
+  });
 }
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  
   if (req.url === '/') {
     res.end('Hello Holberton School!');
-  }
-  
-  if (req.url === '/students') {
-    res.write('This is the list of our students\n');
-    try {
-      const data = countStudents('database.csv');
-      res.write(`Number of students: ${data.NUMBER_OF_STUDENTS}\n`);
-      res.write(`Number of students in CS: ${data.csNames.length}. List: ${data.csNames.join(', ')}\n`);
-      res.write(`Number of students in SWE: ${data.sweNames.length}. List: ${data.sweNames.join(', ')}`);
-      res.end();
-    } catch (error) {
-      res.statusCode = 404;
-      res.end(error.message);
-    }
+  } else if (req.url === '/students') {
+    countStudents(fileName)
+      .then(responseBody => {
+        res.end(responseBody);
+      })
+      .catch(error => {
+        res.end(`This is the list of our students\n${error.message}`);
+      });
   }
 });
 
-app.listen(1245);
+const port = 1245;
+app.listen(port, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
 module.exports = app;
