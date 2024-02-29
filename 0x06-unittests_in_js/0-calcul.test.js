37462e4209e8e81f38c79be413effc122af53f06
OUALIID <walidelhadim8@gmail.com>
2024-02-29 18:36:35 +0100

    Add test cases for calculateNumber function

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -6,6 +6,5 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
     assert.strictEqual(calculateNumber(1, 3.7), 5);
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
