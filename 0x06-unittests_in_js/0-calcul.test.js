e6df5647131ae33672bbdf946ca52e390f61be40
OUALIID <walidelhadim8@gmail.com>
2024-02-29 18:38:28 +0100

    Add test cases for calculateNumber functionc

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,10 +1,11 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul.js');
 
-describe('calculateNumber', () => {
-  it('Tests returns the sum of two rounded numbers', () => {
+describe('calculateNumber', function() {
+  it('Tests returns the sum of two rounded numbers', function() {
     assert.strictEqual(calculateNumber(1, 3), 4);
     assert.strictEqual(calculateNumber(1, 3.7), 5);
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
 });
