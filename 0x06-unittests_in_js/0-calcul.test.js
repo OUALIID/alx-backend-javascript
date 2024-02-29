c255577c5058ddf4088ae1aaefd014e817835947
OUALIID <walidelhadim8@gmail.com>
2024-02-29 18:35:10 +0100

    Add test cases for calculateNumber function

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,5 +1,5 @@
 const assert = require('assert');
-const calculateNumber = require('./0-calcul');
+const calculateNumber = require('./0-calcul.js');
 
 describe('calculateNumber', () => {
   it('Tests returns the sum of two rounded numbers', () => {
