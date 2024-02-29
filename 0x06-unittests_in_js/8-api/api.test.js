508509b5f9e6f624eeb48a9a1bfa4c5c00571867
OUALIID <walidelhadim8@gmail.com>
2024-02-29 23:48:34 +0100

    Implemented an Express app listening on port 7865 with a route for the index page

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,12 +1,26 @@
-const request = require('supertest');
-const app = require('./api');
+const request = require('request');
+const { expect } = require('chai');
+const sinon = require('sinon');
 
-describe('Index page', function () {
-  it('responds with status code 200', (done) => {
-    request(app).get('/').expect(200, done);
+describe('apiTesting', () => {
+  it('The test should return the correct status code', (done) => {
+    request('http://localhost:7865/', (error, res) => {
+      expect(res.status, 200);
+      done();
+    });
   });
-
-  it('responds with the correct message', (done) => {
-    request(app).get('/').expect('Welcome to the payment system', done);
+  it('The test should return the correct result', (done) => {
+    request('http://localhost:7865/', (error, res) => {
+      expect(res.body).to.deep.equal('Welcome to the payment system');
+      done();
+    });
+  });
+  it('Testing The correct message should be logged to the console', (done) => {
+    request('http://localhost:7865/', (error, res) => {
+	  const consoleLog = sinon.spy(console, 'log');
+      expect(consoleLog.calledWith('API available on localhost port 7865'));
+	  consoleLog.restore();
+      done();
+    });
   });
 });
