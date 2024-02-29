3269aaaed999ed15ceef86b27702567fe250967f
OUALIID <walidelhadim8@gmail.com>
2024-02-29 20:20:54 +0100

    Implemented function to handle SUM, SUBTRACT, and DIVIDE operations with rounded input numbers.

---
+++ 0x06-unittests_in_js/1-calcul.js
@@ -0,0 +1,18 @@
+function calculateNumber(type, a, b) {
+  const A = Math.round(a);
+  const B = Math.round(b);
+
+  switch (type) {
+    case 'SUM':
+      return A + B;
+    case 'SUBTRACT':
+      return A - B;
+    case 'DIVIDE':
+      if (B === 0) {
+        return 'Error';
+      }
+      return A / B;
+  }
+};
+
+module.exports = calculateNumber;
