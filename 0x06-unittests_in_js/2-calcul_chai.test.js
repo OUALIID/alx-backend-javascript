f3d49d0fd5d3857cbf7e623378bc7342ce77b42c
OUALIID <walidelhadim8@gmail.com>
2024-02-29 20:57:58 +0100

    Add test cases for calculateNumber function

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,20 +1,32 @@
-const expect = require('chai').expect;
+const { expect } = require('chai');
 const calculateNumber = require('./2-calcul_chai');
 
-describe('calculateNumber', function () {
-    it('Tests return the sum of two rounded numbers of type SUM', function () {
-        expect(calculateNumber('SUM', 1.4, 4.5).to.equal(6));
-    });
-  
-    it('Tests returns the difference between two rounded numbers of type SUBTRACT', function () {
-        expect(calculateNumber('SUBTRACT', 1.4, 4.5).to.equal(-4));
-    });
-  
-    it('The tests return the division of two rounded numbers of type DIVIDE', function () {
-        expect(calculateNumber('DIVIDE', 1.4, 4.5).to.equal(0.2));
-    });
-  
-    it('Tests return error if the rounded value of b is 0 for type DIVIDE', function () {
-        expect(calculateNumber('DIVIDE', 1.4, 0).to.equal('Error'));
-    });
+describe('calculateNumber', () => {
+  it('Testing normal cases...', () => {
+    expect(calculateNumber('SUM', 1, 3)).to.equal(4);
+    expect(calculateNumber('SUBTRACT', 5, 3.7)).to.equal(1);
+    expect(calculateNumber('DIVIDE', 8.2, 3.7)).to.be.closeTo(2.216, 0.001);
+    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.be.closeTo(0.311, 0.001);
   });
+
+  it('Testing division by zero...', () => {
+    expect(calculateNumber('DIVIDE', 1, 0)).to.equal('Error');
+  });
+
+  it('Testing operations with constants...', () => {
+    expect(calculateNumber('SUM', Math.E, Math.PI)).to.be.closeTo(5.86, 0.01);
+    expect(calculateNumber('SUBTRACT', Math.PI, Math.E)).to.be.closeTo(0.42, 0.01);
+    expect(calculateNumber('DIVIDE', Math.E, Math.PI)).to.be.closeTo(0.86, 0.01);
+  });
+
+  it('Testing negative numbers...', () => {
+    expect(calculateNumber('SUM', -1, 3)).to.equal(2);
+    expect(calculateNumber('SUBTRACT', 5, -3.7)).to.equal(9);
+  });
+
+  it('Testing large numbers...', () => {
+    expect(calculateNumber('SUM', 1000000, 2000000)).to.equal(3000000);
+    expect(calculateNumber('SUBTRACT', 1000000, 999999)).to.equal(1);
+    expect(calculateNumber('DIVIDE', 9999999, 3)).to.be.closeTo(3333333, 1);
+  });
+});
