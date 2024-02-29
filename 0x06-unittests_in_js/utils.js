94793473d9c0994e0ebbb839ff4ea89a22693ba6
OUALIID <walidelhadim8@gmail.com>
2024-02-29 22:05:39 +0100

    Add Utils module with calculateNumber function

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,20 @@
+const Utils = {
+  calculateNumber: function (type, a, b) {
+    const A = Math.round(a);
+    const B = Math.round(b);
+
+    switch (type) {
+      case 'SUM':
+        return A + B;
+      case 'SUBTRACT':
+        return A - B;
+      case 'DIVIDE':
+        if (B === 0) {
+          return 'Error';
+        }
+        return A / B;
+    }
+  },
+};
+
+module.exports = Utils;
