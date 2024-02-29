ccc3b48a79b4e1a8eafe639ee44e556db5d7bf91
OUALIID <walidelhadim8@gmail.com>
2024-02-29 20:55:35 +0100

    Add test cases for calculateNumber function

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,10 +1,20 @@
+const expect = require('chai').expect;
 const calculateNumber = require('./2-calcul_chai');
 
-describe('calculateNumber', () => {
-  it('should return the sum of two rounded numbers', () => {
-    expect(calculateNumber('SUM', 1, 3)).to.equal(4);
-    expect(calculateNumber('SUBTRACT', 1, 3.7)).to.equal(-3);
-    expect(calculateNumber('DIVIDE', 1.2, 3.7)).to.be.closeTo(0.324, 0.001);
-    expect(calculateNumber('DIVIDE', 1.5, 0)).to.equal('Error');
+describe('calculateNumber', function () {
+    it('Tests return the sum of two rounded numbers of type SUM', function () {
+        expect(calculateNumber('SUM', 1.4, 4.5).to.equal(6));
+    });
+  
+    it('Tests returns the difference between two rounded numbers of type SUBTRACT', function () {
+        expect(calculateNumber('SUBTRACT', 1.4, 4.5).to.equal(-4));
+    });
+  
+    it('The tests return the division of two rounded numbers of type DIVIDE', function () {
+        expect(calculateNumber('DIVIDE', 1.4, 4.5).to.equal(0.2));
+    });
+  
+    it('Tests return error if the rounded value of b is 0 for type DIVIDE', function () {
+        expect(calculateNumber('DIVIDE', 1.4, 0).to.equal('Error'));
+    });
   });
-});
