2bc432e80ef4f9fc203dca575a09661a56972160
OUALIID <walidelhadim8@gmail.com>
2024-02-29 20:26:09 +0100

    Add test cases for calculateNumber function

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -8,11 +8,14 @@ describe('calculateNumber', function() {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
-  it('Tests returns the sum of two rounded numbers', () => {
+  it('Tests returns the sum of two rounded numbers', function() {
     assert.equal(calculateNumber(3, -7), -4);
     assert.equal(calculateNumber(-1, 2), 1);
   });
-  it('Tests returns the sum of two rounded numbers', () => {
+  it('Tests returns the sum of two rounded numbers', function() {
+    assert.equal(calculateNumber(Math.E, Math.PI), 6);
+  });
+  it('Tests returns the sum of two rounded numbers', function() {
     assert.equal(calculateNumber(11.4, 0), 11);
   });
 });
