8940a7e94b7d83163eb9ee961a938c0da3a6a1b3
OUALIID <walidelhadim8@gmail.com>
2024-03-01 00:16:44 +0100

    Added tests for API endpoints and cart page

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -2,54 +2,35 @@ const request = require('request');
 const { expect } = require('chai');
 const sinon = require('sinon');
 
+/* eslint-disable */
 describe('apiTesting /', () => {
-  it('The test should return the correct status code', (done) => {
+  it('Testing correct statusCode...', (done) => {
     request('http://localhost:7865/', (error, response) => {
       expect(response.statusCode).to.equal(200);
-      done();
-    });
-  });
-  
-  it('The test should return the correct result', (done) => {
-    request('http://localhost:7865/', (error, response) => {
-      expect(response.body).to.deep.equal('Welcome to the payment system');
-      done();
-    });
-  });
-  
-  it('Testing The correct message should be logged to the console', (done) => {
-    request('http://localhost:7865/', (error, response) => {
+      expect(response.body).to.equal('Welcome to the payment system');
       const consoleLog = sinon.spy(console, 'log');
-      expect(consoleLog.calledWith('API available on localhost port 7865')).to.be.true;
+      expect(consoleLog.calledWith('API available on localhost port 7865'));
       consoleLog.restore();
       done();
     });
   });
 });
 
-describe('apiTesting /cart/:id', () => {
-  it('responds with status code 200 for a valid cart ID', (done) => {
+describe('apiTesting /cart/:ip', () => {
+  it('Testing correct id type...', (done) => {
     request('http://localhost:7865/cart/1', (error, response) => {
       expect(response.statusCode).to.equal(200);
+      expect(response.body).to.equal('Payment methods for cart 1');
       done();
     });
   });
-  
-  it('responds with status code 404 for an invalid cart ID', (done) => {
+  it('Testing wrong id type...', (done) => {
     request('http://localhost:7865/cart/hello', (error, response) => {
       expect(response.statusCode).to.equal(404);
       done();
     });
   });
-  
-  it('returns the correct payment methods for a valid cart ID', (done) => {
-    request('http://localhost:7865/cart/1', (error, response) => {
-      expect(response.body).to.equal('Payment methods for cart 1');
-      done();
-    });
-  });
-  
-  it('responds with status code 404 when cart ID is missing', (done) => {
+  it('Testing correct content...', (done) => {
     request('http://localhost:7865/cart', (error, response) => {
       expect(response.statusCode).to.equal(404);
       done();
